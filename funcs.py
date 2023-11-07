07a218a8165f1d49a53ec2abd1b7db53181f279c
Juan Duarte <juanmaduarte2003@gmail.com>
2023-11-07 09:47:34 -0500

    Significancia test

--- funcs.py
+++ funcs.py
@@ -9,129 +9,172 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import DM_HEP_AN as dm
 from math import pi
+
 hep.style.use("CMS")
-#%matplotlib inline
 plt.ioff()
 
-def PlotCinematicVariable(data, variable, suptitle, njets=4, save=True, plot=False, folder='Plots/', dpi=500):
-    fig, ax = plt.subplots(2,2)
+
+def PlotCinematicVariable(
+    data, variable, suptitle, njets=4, save=True, plot=False, folder="Plots/", dpi=500
+):
+    fig, ax = plt.subplots(2, 2)
 
     axs = {
-        0: ax[0,0],
-        1: ax[0,1],
-        2: ax[1,0],
-        3: ax[1,1],
+        0: ax[0, 0],
+        1: ax[0, 1],
+        2: ax[1, 0],
+        3: ax[1, 1],
     }
 
     for i in range(njets):
-        rango = np.linspace(data[f'jet_{variable}{i}'].min(), data[f'jet_{variable}{i}'].max())
-        axs[i].hist(data[f'jet_{variable}{i}'], bins = rango, density = True)
-        axs[i].set_title(f'Jet {i}',fontsize=15)
+        rango = np.linspace(
+            data[f"jet_{variable}{i}"].min(), data[f"jet_{variable}{i}"].max()
+        )
+        axs[i].hist(data[f"jet_{variable}{i}"], bins=rango, density=True)
+        axs[i].set_title(f"Jet {i}", fontsize=15)
         axs[i].grid()
-        axs[i].tick_params(axis='x',labelsize=12)
-        axs[i].tick_params(axis='y',labelsize=12)
+        axs[i].tick_params(axis="x", labelsize=12)
+        axs[i].tick_params(axis="y", labelsize=12)
 
     plt.suptitle(suptitle, fontsize=30)
 
-    if plot: plt.show()
-    if save: plt.savefig(f'{folder}{variable}.png',dpi=dpi)
+    if plot:
+        plt.show()
+    if save:
+        plt.savefig(f"{folder}{variable}.png", dpi=dpi)
     plt.close(fig)
 
-def PlotEtaPhiPlane(data, n_jets=4, save=True, plot=False, folder='Plots/', dpi=500):
-    fig, ax = plt.subplots(2,2)
+
+def PlotEtaPhiPlane(data, n_jets=4, save=True, plot=False, folder="Plots/", dpi=500):
+    fig, ax = plt.subplots(2, 2)
 
     axs = {
-        0: ax[0,0],
-        1: ax[0,1],
-        2: ax[1,0],
-        3: ax[1,1],
+        0: ax[0, 0],
+        1: ax[0, 1],
+        2: ax[1, 0],
+        3: ax[1, 1],
     }
 
     for i in range(n_jets):
-        rango1 = np.linspace(data[f'jet_eta{i}'].min(), data[f'jet_eta{i}'].max())
-        rango2 = np.linspace(data[f'jet_phi{i}'].min(), data[f'jet_phi{i}'].max())
-        axs[i].hist2d(data[f'jet_eta{i}'],data[f'jet_phi{i}'], bins = [rango1,rango2], density = True)
-        axs[i].set_title(f'Jet {i}',fontsize=15)
+        rango1 = np.linspace(data[f"jet_eta{i}"].min(), data[f"jet_eta{i}"].max())
+        rango2 = np.linspace(data[f"jet_phi{i}"].min(), data[f"jet_phi{i}"].max())
+        axs[i].hist2d(
+            data[f"jet_eta{i}"],
+            data[f"jet_phi{i}"],
+            bins=[rango1, rango2],
+            density=True,
+        )
+        axs[i].set_title(f"Jet {i}", fontsize=15)
         axs[i].grid()
-        axs[i].set_xlabel(r'$\eta$',fontsize=15)
-        axs[i].set_ylabel(r'$\phi$',fontsize=15)
-        axs[i].tick_params(axis='x',labelsize=12)
-        axs[i].tick_params(axis='y',labelsize=12)
-    
-    if plot: plt.show()
-    if save: plt.savefig(f'{folder}etaphiplane.png',dpi=dpi)
-    plt.close(fig)
-    
+        axs[i].set_xlabel(r"$\eta$", fontsize=15)
+        axs[i].set_ylabel(r"$\phi$", fontsize=15)
+        axs[i].tick_params(axis="x", labelsize=12)
+        axs[i].tick_params(axis="y", labelsize=12)
 
-def PlotMissingETVariable(data, save=True, plot=False, folder='Plots/', dpi=500):
+    if plot:
+        plt.show()
+    if save:
+        plt.savefig(f"{folder}etaphiplane.png", dpi=dpi)
+    plt.close(fig)
+
+
+def PlotMissingETVariable(data, save=True, plot=False, folder="Plots/", dpi=500):
     fig, ax = plt.subplots(2)
 
-    rango = np.linspace(data['missinget_met'].min(), data['missinget_met'].max())
-    ax[0].hist(data['missinget_met'], bins = rango, density=True)
-    ax[0].set_title(r'$\left|E_{T}^{miss}\right|$', fontsize=15)
+    rango = np.linspace(data["missinget_met"].min(), data["missinget_met"].max())
+    ax[0].hist(data["missinget_met"], bins=rango, density=True)
+    ax[0].set_title(r"$\left|E_{T}^{miss}\right|$", fontsize=15)
     ax[0].grid()
-    ax[0].tick_params(axis='x',labelsize=12)
-    ax[0].tick_params(axis='y',labelsize=12)
+    ax[0].tick_params(axis="x", labelsize=12)
+    ax[0].tick_params(axis="y", labelsize=12)
 
-    rango = np.linspace(data['missinget_phi'].min(), data['missinget_phi'].max())
-    ax[1].hist(data['missinget_phi'], bins = rango, density=True)
-    ax[1].set_title(r'$\phi_{E_{T}^{miss}}$', fontsize=15)
+    rango = np.linspace(data["missinget_phi"].min(), data["missinget_phi"].max())
+    ax[1].hist(data["missinget_phi"], bins=rango, density=True)
+    ax[1].set_title(r"$\phi_{E_{T}^{miss}}$", fontsize=15)
     ax[1].grid()
-    ax[1].tick_params(axis='x',labelsize=12)
-    ax[1].tick_params(axis='y',labelsize=12)
+    ax[1].tick_params(axis="x", labelsize=12)
+    ax[1].tick_params(axis="y", labelsize=12)
 
-    if plot: plt.show()
-    if save: plt.savefig(f'{folder}missinget.png',dpi=dpi)
+    if plot:
+        plt.show()
+    if save:
+        plt.savefig(f"{folder}missinget.png", dpi=dpi)
     plt.close(fig)
+
 
 def makePlots(data, folder, save=True, plot=False):
     # pt
-    PlotCinematicVariable(data, 'pt', r'$p_{T}$', folder=folder, save=save, plot=plot)
+    PlotCinematicVariable(data, "pt", r"$p_{T}$", folder=folder, save=save, plot=plot)
     # eta
-    PlotCinematicVariable(data, 'eta', r'$\eta$', folder=folder, save=save, plot=plot)
+    PlotCinematicVariable(data, "eta", r"$\eta$", folder=folder, save=save, plot=plot)
     # phi
-    PlotCinematicVariable(data, 'phi', r'$\phi$', folder=folder, save=save, plot=plot)
+    PlotCinematicVariable(data, "phi", r"$\phi$", folder=folder, save=save, plot=plot)
     # mass
-    PlotCinematicVariable(data, 'mass', r'$m$', folder=folder, save=save, plot=plot)
+    PlotCinematicVariable(data, "mass", r"$m$", folder=folder, save=save, plot=plot)
     # Eta Phi Plane
     PlotEtaPhiPlane(data, folder=folder, save=save, plot=plot)
     # missinget
     PlotMissingETVariable(data, folder=folder, save=save, plot=plot)
 
+
 def invariant_mass(row):
-    m = np.sqrt(2 * row['jet_pt0'] * row['jet_pt1'] * \
-                (np.cosh(row['jet_eta0'] - row['jet_eta1']) - \
-                np.cosh(row['jet_phi0'] - row['jet_phi1'])))
+    m = np.sqrt(
+        2
+        * row["jet_pt0"]
+        * row["jet_pt1"]
+        * (
+            np.cosh(row["jet_eta0"] - row["jet_eta1"])
+            - np.cosh(row["jet_phi0"] - row["jet_phi1"])
+        )
+    )
     return m
 
+
 def azimuthal_difference(row):
-    deltaPhi = abs(row['jet_phi0'] - row['jet_phi1'])
+    deltaPhi = abs(row["jet_phi0"] - row["jet_phi1"])
     if deltaPhi > np.pi:
-        deltaPhi = abs(deltaPhi - 2*np.pi)
+        deltaPhi = abs(deltaPhi - 2 * np.pi)
     return deltaPhi
 
+
 def pseudorapidity_separation(row):
-    deltaEta = abs(row['jet_eta0'] - row['jet_eta1'])
+    deltaEta = abs(row["jet_eta0"] - row["jet_eta1"])
     return deltaEta
 
+
 def total_henergy(row, n_jets=4):
-    H = sum([row['jet_pt{i}'] for i in range(n_jets)])
+    H = sum([row["jet_pt{i}"] for i in range(n_jets)])
     return H
 
-def plotObservable(datas, names, variable, save=True, plot=False, folder='Plots/', dpi=500, selection=False, query=''):
+
+def plotObservable(
+    datas,
+    names,
+    variable,
+    save=True,
+    plot=False,
+    folder="Plots/",
+    dpi=500,
+    selection=False,
+    query="",
+):
     numDatas = len(datas)
 
     if selection:
         for i in range(numDatas):
-            #if names[i] == 'Z+Jets' or names[i] == 'W+Jets':
+            # if names[i] == 'Z+Jets' or names[i] == 'W+Jets':
             #    continue
-            #else:
+            # else:
             datas[i].query(query, inplace=True)
 
     variableDict = {
-        'Azim_diff' : [azimuthal_difference, r'$\left|\Delta\phi\right|$', (0, np.pi)],
-        'Inv_mass' : [invariant_mass, r'$m_{jj}$ [GeV]', (0,3000)],
-        'Pseudorapidity' : [pseudorapidity_separation, r'$\left|\Delta\eta\right|$', (0,10)]
+        "Azim_diff": [azimuthal_difference, r"$\left|\Delta\phi\right|$", (0, np.pi)],
+        "Inv_mass": [invariant_mass, r"$m_{jj}$ [GeV]", (0, 3000)],
+        "Pseudorapidity": [
+            pseudorapidity_separation,
+            r"$\left|\Delta\eta\right|$",
+            (0, 10),
+        ],
     }
 
     fig = plt.figure()
@@ -140,39 +183,59 @@ def plotObservable(datas, names, variable, save=True, plot=False, folder='Plots/
     majorValue = 0
 
     for i in range(numDatas):
-        if names[i] == 'Z+Jets' or names[i] == 'W+Jets' :
+        if names[i] == "Z+Jets" or names[i] == "W+Jets":
             if not variable in datas[i].columns:
                 datas[i][variable] = datas[i].apply(variableDict[variable][0], axis=1)
             rango = np.linspace(datas[i][variable].min(), datas[i][variable].max())
-            #color = next(ax._get_lines.prop_cycler)["color"]
-            #ax.hist(datas[i][variable], bins=rango, density=True, color=color, edgecolor=color, fc="None", lw=1, label=names[i])
-            yvalues, bins, hist = ax.hist(datas[i][variable], bins=rango, density=True, label=names[i])
+            # color = next(ax._get_lines.prop_cycler)["color"]
+            # ax.hist(datas[i][variable], bins=rango, density=True, color=color, edgecolor=color, fc="None", lw=1, label=names[i])
+            yvalues, bins, hist = ax.hist(
+                datas[i][variable], bins=rango, density=True, label=names[i]
+            )
 
         else:
             if not variable in datas[i].columns:
                 datas[i][variable] = datas[i].apply(variableDict[variable][0], axis=1)
             rango = np.linspace(datas[i][variable].min(), datas[i][variable].max())
             color = next(ax._get_lines.prop_cycler)["color"]
-            yvalues, bins, hist = ax.hist(datas[i][variable], bins=rango, density=True, color=color, edgecolor=color, fc="None", lw=1, label=names[i])
-        
+            yvalues, bins, hist = ax.hist(
+                datas[i][variable],
+                bins=rango,
+                density=True,
+                color=color,
+                edgecolor=color,
+                fc="None",
+                lw=3,
+                label=names[i],
+            )
+
         # Normalization to total number of events
         for item in hist:
-            item.set_height(item.get_height()/sum(yvalues))
-        
-        majorValue = yvalues.max()/sum(yvalues) if yvalues.max()/sum(yvalues) > majorValue else majorValue
-    
+            item.set_height(item.get_height() / sum(yvalues))
+
+        majorValue = (
+            yvalues.max() / sum(yvalues)
+            if yvalues.max() / sum(yvalues) > majorValue
+            else majorValue
+        )
+
     ax.legend(fontsize=20)
-    ax.set_title(variableDict[variable][1], fontsize=25)
+    ax.set_xlabel(variableDict[variable][1], fontsize=25)
     ax.grid()
-    ax.tick_params(axis='x',labelsize=20)
-    ax.tick_params(axis='y',labelsize=20)
+    ax.tick_params(axis="x", labelsize=20)
+    ax.tick_params(axis="y", labelsize=20)
     ax.set_xlim(variableDict[variable][2])
-    ax.set_ylim(0,majorValue)
+    ax.set_ylim(0, majorValue)
+    ax.set_ylabel("a.u.")
+
+    hep.cms.lumitext(text="(13 TeV)")
 
     if selection:
-        f'{folder}{variable}_withQuery.png'
+        f"{folder}{variable}_withQuery.png"
     else:
-        filePath = f'{folder}{variable}.png'
-    if save: plt.savefig(filePath,dpi=dpi)
-    if plot: plt.show()
+        filePath = f"{folder}{variable}.png"
+    if save:
+        plt.savefig(filePath, dpi=dpi)
+    if plot:
+        plt.show()
     plt.close(fig)
